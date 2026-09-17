2a57a79b92b7bc862f2ee1b4ab60d09be0398b9d
Ayush Kumar <ayushkkumar840@gmail.com>
2026-09-17 14:46:45 +0530

    practice

--- Multi-Threading/src/com/kodewala/multithrea/class1/Driver6.java
+++ Multi-Threading/src/com/kodewala/multithrea/class1/Driver6.java
@@ -1,5 +1,21 @@
 package com.kodewala.multithrea.class1;
 
 public class Driver6 {
+	public static void main(String[] args) {
 
+		MyThread t1 = new MyThread();
+		MyThread t2 = t1;
+
+		t1.start();
+		//t2.start();
+	}
+
+}
+
+class MyThread extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println("Executing");
+	}
 }
